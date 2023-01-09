a0063d8e47c127cd603a4446e964eb12f16f92de
Ira Strubhar <taylor.strubhar@gmail.com>
2023-01-09 12:40:10 -0800

    updated about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,8 +9,8 @@ function About() {
             My name is Ira "Taylor" Strubhar and I am a full stack web developer leveraging a customer service, sales and art background to build
 a more intuitive user experience on the web. I earned a certificate in full stack
 development from the University of Minnesota, with skills in JavaScript,
-MongoDB, CSS, React.js, and responsive web design. I'm known as an organized, inventive
-and engaged team member passionate about developing apps, with a focus on
+MongoDB, Express, Node, React.js, and responsive web design. I'm known as an organized, inventive
+and engaged team member passionate about developing apps with a focus on
 mobile-first design, solid UX and WCAG compliance.
         </p>
     </section>
